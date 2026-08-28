079a9345997c1d4c4e117ded5550592049e42562
Vishalrs2007 <vishalrs1023@gmail.com>
2026-08-28 12:33:12 +0530

    Add files via upload

--- linklist.c
+++ linklist.c
@@ -1,9 +1,37 @@
-#include <stdio.h>
-#include<stdlib.h>
-
-struct node{
-int data;
-struct node *prev;
-struct node *next;
+ #include<stdio.h>
+ #include<stdlib.h>
+ 
+ struct Node{
+ int data;
+ struct Node *next;
 };
-struct node *head = NULL;
+
+int main(){
+struct Node *head,*second,*third;
+struct Node *temp;
+
+head = (struct Node *)malloc(sizeof(struct Node));
+second = (struct Node *)malloc(sizeof(struct Node));
+third = (struct Node *)malloc(sizeof(struct Node));
+
+head->data = 10;
+head->next = second;
+
+second->data = 20;
+second->next = third;
+
+third->data = 30;
+third->next = NULL;
+ temp = head;
+
+printf("Linked list:");
+
+while(temp!=NULL){
+printf("%d ->",temp->data);
+
+temp = temp-> next;
+}
+printf("NULL\n");
+
+return 0;
+}
